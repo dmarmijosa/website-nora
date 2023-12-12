0f8eb4d9988c4a25711f5f28454177210f4ad06f
Danny Armijos <daniel2667@hotmail.es>
2023-12-12 22:44:08 +0100

    diciembre efecto

--- js/script.js
+++ js/script.js
@@ -101,3 +101,25 @@ document.getElementById('sendMailButton').addEventListener('click', function() {
 });
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    if (new Date().getMonth() === 11) { // Diciembre es el mes 11 (0 es Enero)
+        createSnowflakes();
+    }
+});
+
+function createSnowflakes() {
+    const colors = ['#FFC0CB', '#ADD8E6', '#90EE90', '#FFFFE0', '#FFA07A']; // Colores de las bolas de nieve
+
+    for (let i = 0; i < 100; i++) {
+        const snowflake = document.createElement('div');
+        snowflake.classList.add('snowflake');
+        snowflake.textContent = '●'; // Carácter para la bola de nieve
+        snowflake.style.fontSize = `${Math.random() * 20 + 10}px`; // Tamaño aleatorio
+        snowflake.style.color = colors[Math.floor(Math.random() * colors.length)]; // Color aleatorio
+        snowflake.style.left = `${Math.random() * 100}vw`; // Posición horizontal aleatoria
+        snowflake.style.animationDelay = `${Math.random() * 5}s`; // Retraso aleatorio
+        snowflake.style.animationDuration = `${Math.random() * 5 + 5}s`; // Duración aleatoria
+
+        document.body.appendChild(snowflake);
+    }
+}
